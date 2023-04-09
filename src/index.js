2f0b44825f5fe5ffb0601098ca18ec472b9b9dd2
secure-kos <secure.kos@gmail.com>
2023-04-09 17:23:18 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 require("./db/mongoose");
+const cors = require("cors");
+
 const { userRouter } = require("./routes/userRouter");
 const { taskRouter } = require("./routes/taskRouter");
 
 const app = express();
 const port = process.env.PORT | 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
 
 app.listen(port, () => {
   console.log("Server is starting");
