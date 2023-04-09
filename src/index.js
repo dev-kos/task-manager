e4911ecc37eeb2c2f91e8060ac289041ef40eb47
secure-kos <secure.kos@gmail.com>
2023-04-09 18:02:20 +0300

    asdf

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ const { taskRouter } = require("./routes/taskRouter");
 const app = express();
 const port = process.env.PORT | 3000;
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  preflightContinue: true,
-  optionsSuccessStatus: 204,
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(userRouter);
