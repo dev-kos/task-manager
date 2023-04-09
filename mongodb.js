dca875e7cd25b842ab73478f4026df4e5119e449
secure-kos <secure.kos@gmail.com>
2023-04-09 17:06:43 +0300

    fixed mongodb url

--- mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const { TaskModel } = require("./models/Task");
-const { UserModel } = require("./models/User");
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("Connected!"));
-
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/task-manager-api")
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log("Connected to database!"));
