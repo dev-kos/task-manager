f8e57388168330e7a17381d2561380a5a932e004
secure-kos <secure.kos@gmail.com>
2023-04-09 17:56:56 +0300

    asdf

--- src/index.js
+++ src/index.js
@@ -11,13 +11,16 @@ const port = process.env.PORT | 3000;
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
