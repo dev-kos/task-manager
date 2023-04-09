2bd44488fd50f01e259122d26beea13276b0d9ea
secure-kos <secure.kos@gmail.com>
2023-04-09 18:00:56 +0300

    added

--- src/index.js
+++ src/index.js
@@ -8,18 +8,12 @@ const { taskRouter } = require("./routes/taskRouter");
 const app = express();
 const port = process.env.PORT | 3000;
 
-app.use(cors());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.options("*", cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  preflightContinue: true,
+  optionsSuccessStatus: 204,
+}));
 
 app.use(express.json());
 app.use(userRouter);
